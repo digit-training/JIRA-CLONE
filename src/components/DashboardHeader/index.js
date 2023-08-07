cd75bc05226ea1132fcec2522f531fcfb2436742
Rishabh-egov <rishabh.jain@egovernments.org>
2023-08-07 17:08:32 +0530

    Feat : added dashboard

---
+++ src/components/DashboardHeader/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid, colors} from "@mui/material";
+// import TicketHolder from "../TicketHolder";
+const DashboardHeader = () => (
+  <div>
+    <Grid container spacing={2} style={{ backgroundColor: colors.blueGrey[50], padding: "16px" }} >
+      <Grid item md={12} >
+        <Grid container spacing={2} >
+            {["To Do","In Progress","Reopened","On Hold","To Review","In Review","Invalid","Dropped","Signed Off"].map( (item) => {
+                 return (
+                   <>
+                    <Grid item xs={1.2} style={{backgroundColor:colors.amber[100]}} margin={{margin:"6px"}} key={item} padding={{padding:"20px"}}>  
+                            <span>{`${item}`}</span>
+                    </Grid>
+                   </>
+                 )
+            } )}
+             {["To Do","In Progress","Reopened","On Hold","To Review","In Review","Invalid","Dropped","Signed Off"].map( (item) => {
+                 return (
+                   <>
+                    <Grid item xs={1.2} style={{backgroundColor:colors.grey[100] , height:"90vh"}} margin={{margin:"6px"}} key={item} padding={{padding:"20px"}}>  
+                    </Grid>
+                   </>
+                 )
+            } )}
+            </Grid>
+          </Grid>
+     </Grid>
+  </div>
+);
+
+export default DashboardHeader;
