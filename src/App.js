eef9fc3af7af00e6b3d8e968652916f54aa4f97c
ansh-egov <ansh.goyal@egovernments.org>
2023-08-08 14:36:16 +0530

    Fixed Popup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
 import PopupScreen from "./components/CreateIssue";
-import Sidebar from './Components/Sidebar';
-import './App.css';
-import Dashboard from './pages/dashboard';
-import Navbar from './components/Navbar';
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
+import Dashboard from "./pages/dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [isPopupOpen, setPopupOpen] = useState(false);
-
-  const handleOpenPopup = () => {
-    setPopupOpen(true);
-  };
-
-  const handleClosePopup = () => {
-    setPopupOpen(false);
-  };
   return (
     <div className="App">
-      <h1>Popup Screen Example</h1>
-      <Button variant="contained" color="primary" onClick={handleOpenPopup}>
-        Open Popup
-      </Button>
-      <PopupScreen open={isPopupOpen} onClose={handleClosePopup} />
-      <Navbar/>
-      <Sidebar/>
-      <Dashboard/>
+      <Navbar />
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
