d8ff1c11a58ae53fa2ecedeb58ef4aa89ea1ae75
Your Name <youremail@domain.com>
2023-08-08 12:49:20 +0530

    added signup form

---
+++ src/components/Navbar/signup/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SignUpForm from "./SignUpForm.js";
+import './style.css'
+
+
+function SignUp(){
+    return(
+        <div>
+          <header className="header">
+            <span><h1 className="head">Welcome To Jira App</h1></span>
+            <span>
+              <h2 className="head">Sign Up</h2>
+            </span>
+      
+
+      </header>
+      <SignUpForm/>
+      </div>
+    );
+
+}
+
+export default SignUp;
