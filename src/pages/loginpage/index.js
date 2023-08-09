ce90e974c99357ec476b7aaa353efe07444634f9
ashish-egov <ashish.tiwari@egovernments.org>
2023-08-09 20:32:37 +0530

    feat: Implement Redux, Issue Cards, and Grid State Handling

--- src/pages/loginpage/index.js
+++ src/pages/loginpage/index.js
@@ -50,94 +50,95 @@ const LoginPage = () => {
 
   };
   return (
-    <div className="login-container">
+    <div className='page-container'>
+      <div className="login-container" >
 
-      <h1> {language === 'en' ? 'Login to Jira' : 'Connexion à Jira'}</h1>
+        <h1> {language === 'en' ? 'Login to Jira' : 'Connexion à Jira'}</h1>
 
-      <form>
-        <div className="input-container">
-          <label className="label">
-            {language === 'en' ? 'Username:' : 'Nom d\'utilisateur :'}
-          </label>
-          <input
-            type="text"
-            value={username}
-            placeholder='Username'
-            className='input-field'
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div className="input-container">
-          <label className="label">
-            {language === 'en' ? 'Email:' : 'Email :'}
-          </label>
-          <input
-            type="text"
-            value={email}
-            placeholder='Email Address'
-            className='input-field'
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div className="input-container">
-          <label className="label">
-            {language === 'en' ? 'Password:' : 'Mot de passe :'}
-          </label>
-          <input
-            type="password"
-            value={password}
-            placeholder='Password'
-            className='input-field'
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div className="input-container">
-          <div className="keep-logged-in">
-            <input
-              className="keep-logged-in-checkbox"
-              type="checkbox"
-              id="keepLoggedIn"
-              checked={keepLoggedIn}
-              onChange={handleKeepLoggedInChange}
-            />
-            <label htmlFor="keepLoggedIn">
-              &nbsp;
-              {language === 'en' ? keepLoginEng : ' Rester connecté '}
+        <form>
+          <div className="input-container">
+            <label className="label">
+              {language === 'en' ? 'Username:' : 'Nom d\'utilisateur :'}
             </label>
+            <input
+              type="text"
+              value={username}
+              placeholder='Username'
+              className='input-field'
+              onChange={handleUsernameChange}
+            />
           </div>
-        </div>
-        <div className="login-button-container">
-          <button className="login-button" type="button" onClick={handleSubmit}>
-            {language === 'en' ? 'Login' : 'Se connecter'}
-          </button>
-        </div>
-        <div className='login-button-container'>
-          <p className="login-message">{loginMessage}</p>
-        </div>
+          <div className="input-container">
+            <label className="label">
+              {language === 'en' ? 'Email:' : 'Email :'}
+            </label>
+            <input
+              type="text"
+              value={email}
+              placeholder='Email Address'
+              className='input-field'
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="input-container">
+            <label className="label">
+              {language === 'en' ? 'Password:' : 'Mot de passe :'}
+            </label>
+            <input
+              type="password"
+              value={password}
+              placeholder='Password'
+              className='input-field'
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="input-container">
+            <div className="keep-logged-in">
+              <input
+                className="keep-logged-in-checkbox"
+                type="checkbox"
+                id="keepLoggedIn"
+                checked={keepLoggedIn}
+                onChange={handleKeepLoggedInChange}
+              />
+              <label htmlFor="keepLoggedIn">
+                &nbsp;
+                {language === 'en' ? keepLoginEng : ' Rester connecté '}
+              </label>
+            </div>
+          </div>
+          <div className="login-button-container">
+            <button className="login-button" type="button" onClick={handleSubmit}>
+              {language === 'en' ? 'Login' : 'Se connecter'}
+            </button>
+          </div>
+          <div className='login-button-container'>
+            <p className="login-message">{loginMessage}</p>
+          </div>
 
-      </form>
-      <div className="links-container">
-        <a href="/forgot-password">
-          {language === 'en' ? 'Forgot Password?' : 'Mot de passe oublié ?'}
-        </a>
-      </div>
-      <div className="sign-txt">
-        {language === 'en' ? 'Not yet a member? ' : 'Pas encore membre ? '}
-        <a onClick={() => { navigate("/signup") }}>
-          {language === 'en' ? 'Signup now' : 'Inscrivez-vous maintenant'}
-        </a>
-      </div>
-      <p>
-        {language === 'en'
-          ? 'By logging in, you agree to the '
-          : 'En vous , vous acceptez la '}
-        <strong>{language === 'en' ? 'Privacy Policy' : 'Polque confidentialité'}</strong>
-        <br />
-        <br />
+        </form>
+        <div className="links-container">
+          <a href="/forgot-password">
+            {language === 'en' ? 'Forgot Password?' : 'Mot de passe oublié ?'}
+          </a>
+        </div>
+        <div className="sign-txt">
+          <a onClick={() => { navigate("/signup") }}>
+            {language === 'en' ? 'Not yet a member? Signup now' : 'Pas encore membre ? Inscrivez-vous maintenant'}
+          </a>
+        </div>
+        <p>
+          {language === 'en'
+            ? 'By logging in, you agree to the '
+            : 'En vous , vous acceptez la '}
+          <strong>{language === 'en' ? 'Privacy Policy' : 'Polque confidentialité'}</strong>
+          <br />
+          <br />
 
-      </p><button className="lng-button" onClick={toggleLanguage}>
-        {language === 'en' ? 'Switch to French' : 'Passer en anglais'}
-      </button>
+        </p><button className="lng-button" onClick={toggleLanguage}>
+          {language === 'en' ? 'Switch to French' : 'Passer en anglais'}
+        </button>
+      </div>
     </div>
 
   );
