650ce57e6763cc0f7229881e552e033874b31a1b
ansh-egov <ansh.goyal@egovernments.org>
2023-08-09 10:10:05 +0530

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
 import PopupScreen from "./components/CreateIssue";
-import Sidebar from './Components/Sidebar';
-import './App.css';
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
 
-import Dashboard from './pages/dashboard';
-import Navbar from './components/Navbar';
+import Dashboard from "./pages/dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [isPopupOpen, setPopupOpen] = useState(false);
