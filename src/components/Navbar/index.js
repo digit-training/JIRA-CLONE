180134c588ddebbd918ebbf6ac263eba1e623637
ashish-egov <ashish.tiwari@egovernments.org>
2023-08-09 14:52:17 +0530

    feat : improved routings

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -29,7 +29,7 @@ function Navbar() {
   };
   const handleSignupClick = () => {
     // Implement your signup logic here
-    navigate('/signUp')
+    navigate('/signup')
     //alert("You clicked Signup!");
   };
 
@@ -53,7 +53,7 @@ function Navbar() {
 
   return (
     <div id="navbar">
-      <button className="more-options-button" style={{marginLeft:"20px"}} onClick={handleMoreOptionsClick}>
+      <button className="more-options-button" style={{ marginLeft: "20px" }} onClick={handleMoreOptionsClick}>
         <i className="fas fa-ellipsis-v"></i>
       </button>
       <ApiIcon style={{ marginLeft: "3px" }}></ApiIcon>
@@ -87,7 +87,7 @@ function Navbar() {
         <i className="fas fa-search search-icon"></i>
       </div>
       <NotificationsIcon style={{ marginRight: "5px" }}> </NotificationsIcon>
-      <div className="settings-icon" style={{marginRight:"20px"}}>
+      <div className="settings-icon" style={{ marginRight: "20px" }}>
         <i className="fas fa-cog"></i>
       </div>
     </div>
