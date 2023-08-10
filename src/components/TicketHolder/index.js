11c5b32bb8e027149ee20a51bb7fedc7ad2ff2a1
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2023-08-10 13:50:23 +0530

    improved routing (#65)
    Co-authored-by: rachana-egov <rachana.singh@egovernment.org>

--- src/components/TicketHolder/index.js
+++ src/components/TicketHolder/index.js
@@ -4,7 +4,7 @@ import { Grid, colors } from "@mui/material";
 const TicketHolder = () => (
   <div>
     <Grid container spacing={4} style={{ backgroundColor: colors.grey[50] }}>
-      {["To Do", "In Progress", "Reopened", "On Hold", "To Review", "In Review", "Invalid", "Dropped", "Signed Off"].map((item) => (
+      {["To Do", "In Progress", "Reopened", "On Hold", "To Review", "In Review", "Invalid", "Dropped", "Signed Off", "Point"].map((item) => (
         <Grid item xs={3} style={{ backgroundColor: colors.amber[100], margin: "6px", padding: "20px", flexGrow: 1 }} key={item}>
           <span>{item}</span>
         </Grid>
