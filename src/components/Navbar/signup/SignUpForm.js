4b50a5037cc6eba3e207a6527e78604e9511cc73
aaradhya-egov <137176709+aaradhya-egov@users.noreply.github.com>
2023-08-11 11:15:12 +0530

    Updated signup (#69)
    * added hook form
    
    * sign up using hook
    
    * feat:updated signup
    
    ---------
    
    Co-authored-by: Your Name <youremail@domain.com>

--- src/components/Navbar/signup/SignUpForm.js
+++ src/components/Navbar/signup/SignUpForm.js
@@ -3,7 +3,6 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import FormInput from './FormInput';
 import './style.css';
-import { Height } from '@mui/icons-material';
 
 function SignUpForm() {
     const methods = useForm();
@@ -15,53 +14,52 @@ function SignUpForm() {
     };
 
     return (
-        <div style={{ height: "70vh" }}>
-            <div className="form">
-                <FormProvider {...methods}>
-                    <form onSubmit={onSubmit} className="form-body">
-                        <FormInput
-                            name="firstName"
-                            label="First Name"
-                            inputType="text"
-                            validation={{ required: "First name is required" }}
-                        />
-                        <FormInput
-                            name="lastName"
-                            label="Last Name"
-                            inputType="text"
-                        />
-                        <FormInput
-                            name="email"
-                            label="Email"
-                            inputType="email"
-                            validation={{ required: "Email is required", pattern: /^\S+@\S+$/i }}
-                        />
-                        <FormInput
-                            name="password"
-                            label="Password"
-                            inputType="password"
-                            validation={{ required: "Password is required", minLength: 6 }}
-                        />
-                        <FormInput
-                            name="confirmPassword"
-                            label="Confirm Password"
-                            inputType="password"
-                            validation={{
-                                required: "Confirm Password is required",
-                                validate: (value) => value === methods.watch("password") || "Passwords do not match",
-                            }}
-                        />
-                        <div className="footer">
-                            <button className="submit btn" type="submit">Submit</button>
-                            <button className="login btn" onClick={() => navigate('/')}>Login</button>
-                        </div>
-                    </form>
-                </FormProvider>
-            </div>
+        <div className="form">
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit(onSubmit)} className="form-body">
+                    <FormInput
+                        name="firstName"
+                        label="First Name"
+                        inputType="text"
+                        validation={{ required: "First name is required" }}
+                    />
+                    <FormInput
+                        name="lastName"
+                        label="Last Name"
+                        inputType="text"
+                    />
+                    <FormInput
+                        name="email"
+                        label="Email"
+                        inputType="email"
+                        validation={{ required: "Email is required", pattern: /^\S+@\S+$/i }}
+                    />
+                    <FormInput
+                        name="password"
+                        label="Password"
+                        inputType="password"
+                        validation={{ required: "Password is required", minLength: 6 }}
+                    />
+                    <FormInput
+                        name="confirmPassword"
+                        label="Confirm Password"
+                        inputType="password"
+                        validation={{
+                            required: "Confirm Password is required",
+                            validate: (value) => value === methods.watch("password") || "Passwords do not match",
+                        }}
+                    />
+                    <div className="footer">
+                        <button className="submit btn" type="submit">Submit</button>
+                        <button className="login btn" onClick={() => navigate('/')}>Login</button>
+                    </div>
+                </form>
+            </FormProvider>
         </div>
     );
-
 }
 
 export default SignUpForm;
+
+
 
