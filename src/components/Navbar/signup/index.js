3c6b39182ef0a80ad94ec5c8d7538914a923365b
admin1 <nitish@egovernments.org>
2023-08-11 15:19:26 +0530

    made sign up page responsive

--- src/components/Navbar/signup/index.js
+++ src/components/Navbar/signup/index.js
@@ -4,15 +4,15 @@ import './style.css'
 
 
 function SignUp() {
+
+
   return (
     <div>
       <header className="header" style={{ height: "25vh" }}>
-        <span><h1 className="head">Welcome To Jira App</h1></span>
-        <span>
-          <h2 className="head">Sign Up</h2>
+        <span className="flex flex-col items-center h-full justify-center">
+          <h1 className="text-2xl font-semibold mb-1000">Welcome To Jira App</h1>
+          <h2 className="text-2xl font-semibold  text-blue-500">Sign Up</h2>
         </span>
-
-
       </header>
       <SignUpForm />
     </div>
