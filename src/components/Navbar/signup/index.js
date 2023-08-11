2dd57485164ef2b3300949d64aadf3e072c2812c
admin1 <nitish@egovernments.org>
2023-08-11 17:12:07 +0530

    made login page responsive

--- src/components/Navbar/signup/index.js
+++ src/components/Navbar/signup/index.js
@@ -10,7 +10,7 @@ function SignUp() {
     <div>
       <header className="header" style={{ height: "25vh" }}>
         <span className="flex flex-col items-center h-full justify-center">
-          <h1 className="text-2xl font-semibold mb-1000">Welcome To Jira App</h1>
+          <h1 className="text-2xl font-semibold mb-4">Welcome To Jira App</h1>
           <h2 className="text-2xl font-semibold  text-blue-500">Sign Up</h2>
         </span>
       </header>
