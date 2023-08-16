9e2b0cb512de6561565d55633d5fc380432057e5
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2023-08-16 14:27:36 +0530

    Feat: handle CORS error (#105)
    * integrated business Service State
    
    * handled CORS error
    
    ---------
    
    Co-authored-by: rachana-egov <rachana.singh@egovernment.org>

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "/egov-workflow-v2", // Base URL of the API
+  baseURL: "https://thingproxy.freeboard.io/fetch/https://qa.digit.org/egov-workflow-v2", // Base URL of the API
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
     Accept: "application/json, text/plain, */*",
