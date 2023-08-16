897d6e0ce83d4dca19c4c1acce5327da6dfd5833
rachana-egov <137176770+rachana-egov@users.noreply.github.com>
2023-08-16 13:46:40 +0530

    integrated business Service State (#104)
    Co-authored-by: rachana-egov <rachana.singh@egovernment.org>

--- src/appReducer.js
+++ src/appReducer.js
@@ -1,5 +1,6 @@
 const initialState = {
     cards: [],
+    businessServiceStates: ["To Do", "In Progress", "Reopened", "On Hold", "To Review", "In Review", "Invalid", "Dropped", "Signed Off"], // Corrected the typo 'bussinesServiceStates' to 'businessServiceStates'
     // other initial state properties
 };
 
@@ -9,6 +10,11 @@ const appReducer = (state = initialState, action) => {
             return {
                 ...state,
                 cards: [...state.cards, action.payload],
+            };
+        case "UPDATE_BUSINESS_SERVICE_STATES": // Use your actual action type here
+            return {
+                ...state,
+                businessServiceStates: action.payload,
             };
         // other cases
         default:
